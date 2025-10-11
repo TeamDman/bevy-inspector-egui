f02ac24defbf4e1248c24d62a9c14fa9d0378203
TeamDman <TeamDman9201@gmail.com>
2025-10-10 22:44:14 -0400

    Add exit condition and custom entity count to world inspector example

--- crates/bevy-inspector-egui/examples/quick/world_inspector.rs
+++ crates/bevy-inspector-egui/examples/quick/world_inspector.rs
@@ -1,19 +1,37 @@
+use std::env;
+
 use bevy::{input::common_conditions::input_toggle_active, prelude::*};
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
 fn main() {
+    let mut args = env::args();
+    args.next(); // skip executable name
+    let num_extra_entities = args.next()
+        .unwrap_or_default()
+        .parse::<u32>()
+        .unwrap_or(0);
+    let num_ticks = args.next()
+        .unwrap_or_default()
+        .parse::<u32>()
+        .inspect(|n| {
+            println!("The app will exit after {} ticks (frames).", n);
+        })
+        .unwrap_or(u32::MAX);
+    let start = std::time::Instant::now();
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin::default())
         .add_plugins(
             WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
         )
-        .add_systems(Startup, setup)
+        .add_systems(Startup, setup.with_input(num_extra_entities))
+        .add_systems(Update, exit_after_delay.with_input(num_ticks))
         .run();
+    println!("App exited after {:?}", start.elapsed());
 }
-
 /// set up a simple 3D scene
 fn setup(
+    InMut(num_extra_entities): InMut<u32>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -44,4 +62,25 @@ fn setup(
         Camera3d::default(),
         Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
+
+    // extra entities
+    for i in 0..*num_extra_entities {
+        commands.spawn((
+            Name::new(format!("Extra Cube {i}")),
+            Transform::from_xyz(i as f32 * 2.0, 0.5, 2.0),
+            Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+            MeshMaterial3d(materials.add(Color::srgba(0.0, 0.0, 1.0, 0.5))),
+        ));
+    }
+}
+
+fn exit_after_delay(
+    InMut(num_ticks): InMut<u32>,
+    mut exit: MessageWriter<AppExit>,
+) {
+    if *num_ticks == 0 {
+        exit.write(AppExit::default());
+    } else {
+        *num_ticks -= 1;
+    }
 }
